6d51179b9a8d7f5b9fe455e7c371e01710523cc3
Vincent <magicdroidx@gmail.com>
2024-09-09 21:15:28 +0100

    chore: set api_version to 0.5

--- src/endstone_disable_me/plugin.py
+++ src/endstone_disable_me/plugin.py
@@ -1,11 +1,20 @@
 from endstone.plugin import Plugin
-from endstone.event import event_handler, PlayerLoginEvent
+from endstone.event import event_handler, ServerCommandEvent, PlayerLoginEvent
 
 
 class DisableMe(Plugin):
+    api_version = 0.5
+
     def on_enable(self) -> None:
         self.register_events(self)
 
     @event_handler
     def on_player_login(self, event: PlayerLoginEvent) -> None:
         event.player.add_attachment(self, "minecraft.command.me", False)
+
+    @event_handler
+    def on_command_preprocess(self, event: ServerCommandEvent) -> None:
+        command = event.command
+        if command.count("@") >= 5:
+            event.cancelled = True
+            event.sender.send_error_message("You are not allowed to use selectors excessively.")
