b4a5104245edbf2098e3178aab9a2f4b42b011b8
Vincent <magicdroidx@gmail.com>
2024-12-24 13:56:03 +0000

    chore: release v0.1.0

--- src/endstone_disable_me/plugin.py
+++ src/endstone_disable_me/plugin.py
@@ -1,9 +1,20 @@
+import uuid
+
 from endstone.plugin import Plugin
-from endstone.event import event_handler, ServerCommandEvent, PlayerLoginEvent
+from endstone.event import (
+    event_handler,
+    PlayerCommandEvent,
+    PlayerLoginEvent,
+    PlayerQuitEvent,
+)
 
 
 class DisableMe(Plugin):
-    api_version = 0.5
+    api_version = "0.5"
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._spam_counter: dict[uuid.UUID, int] = {}
 
     def on_enable(self) -> None:
         self.register_events(self)
@@ -11,10 +22,21 @@ class DisableMe(Plugin):
     @event_handler
     def on_player_login(self, event: PlayerLoginEvent) -> None:
         event.player.add_attachment(self, "minecraft.command.me", False)
+        self._spam_counter[event.player.unique_id] = 0
 
     @event_handler
-    def on_command_preprocess(self, event: ServerCommandEvent) -> None:
+    def on_player_quit(self, event: PlayerQuitEvent) -> None:
+        del self._spam_counter[event.player.unique_id]
+
+    @event_handler
+    def on_player_command(self, event: PlayerCommandEvent) -> None:
         command = event.command
         if command.count("@") >= 5:
-            event.cancelled = True
-            event.sender.send_error_message("You are not allowed to use selectors excessively.")
+            event.is_cancelled = True
+            event.player.send_error_message(
+                "You are not allowed to use selectors excessively."
+            )
+            self._spam_counter[event.player.unique_id] += 1
+
+            if self._spam_counter[event.player.unique_id] >= 5:
+                event.player.kick("You are not allowed to use selectors excessively.")
